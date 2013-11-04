4c96b3796ac5ff47b967a749f1ebaac7d50c6a50
Nico <nicolas.jouanin@gmail.com>
2013-11-04 22:33:42 +0100

    clean conf

--- conf.py
+++ conf.py
@@ -102,27 +102,16 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = 'default'
-#html_theme = 'bootstrap'
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #html_theme_options = {}
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.
-html_theme_options = {
-    # HTML navbar class (Default: "navbar") to attach to <div> element.
-    # For black navbar, do "navbar navbar-inverse"
-    #'navbar_class': "navbar navbar-inverse",
-
-    # Location of link to source.
-    # Options are "nav" (default), "footer" or anything else to exclude.
-    #'source_link_position': "footer",
-
-}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
